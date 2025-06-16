d2e5dd272a0fa54f4a1ad3087a73e631a81c382b
Spandan2405 <121621850+Spandan2405@users.noreply.github.com>
2025-06-17 00:30:22 +0530

    Update Resume#2.js

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -5,7 +5,7 @@ function Resume() {
   return (
     <section id="Resume">
       <a
-        href="https://drive.google.com/file/d/1XMwg1lPcD7Wq8QNbu7uxyQ6ST4o1FmXv/view?usp=sharing"
+        href="https://drive.google.com/file/d/1i8utiU4sw0v9eeOOYRz8ewUV5SV2_Goo/view?usp=sharing"
         target="_blank"
         rel="noreferrer"
       >
